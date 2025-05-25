8164f96d86302e33672bd5ddbbbce26f817134df
Shahrukh <mohdshahrukh3012@gmail.com>
2025-05-25 21:20:20 +0530

    simple recursion problems

--- mainfile.py
+++ mainfile.py
@@ -1,8 +1,44 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat May 24 15:06:18 2025
+'''
+reversing a string
+base case
+hen length of string is one it retruns the same string
+else
+'''
 
-@author: Shahrukh
-"""
+def reversestring(s):
+    print('entering ith ',s)
+    if len(s)<=1:
+        return s
+    else:
+        k=reversestring(s[1:])+s[0]
+    
+    return k
+        
+'''
+palindrome check
+a string is if its same from foreard and backard
+if size is 1 then its yes
 
-print('Diabeties Medicine Problem')
+'''   
+def checkpalindrome(s):
+    print('entering ith ',s)
+    if len(s)<=1:
+        return True
+    else:
+        c1=s[0]
+        c2=s[-1]
+        if c1 !=c2:
+            k= False
+        else:
+            k=True
+            k=checkpalindrome(s[1:-1])
+            
+    print('leaving ith',s[1:-1])
+    return k
+            
+        
+        
+
+    
+l=checkpalindrome('naman')
+print(l)
